7f8ef5bb5b83f31fb69a6bffb247a3bdac0a6c3b
msegade <miguelrsegade@gmail.com>
2017-06-21 14:07:27 +0200

    New pipeline

---
+++ Fuselage/code_aster/SkinPres/geometry.py
@@ -0,0 +1,45 @@
+#############Print results in current working directory###########
+import os
+currentpath=os.path.join(os.getcwd(), '')
+ExportPATH=currentpath
+##################################################################
+
+
+import sys
+import salome
+
+salome.salome_init()
+theStudy = salome.myStudy
+
+
+###
+### GEOM component
+###
+
+import GEOM
+from salome.geom import geomBuilder
+import math
+import SALOMEDS
+
+geompy = geomBuilder.New(theStudy)
+
+# Parameters
+Diameter = 3.8
+R = Diameter/2.0
+length = 6
+
+O = geompy.MakeVertex(0, 0, 0)
+OX = geompy.MakeVectorDXDYDZ(1, 0, 0)
+OY = geompy.MakeVectorDXDYDZ(0, 1, 0)
+OZ = geompy.MakeVectorDXDYDZ(0, 0, 1)
+Circle = geompy.MakeCircle(O, OZ, R)
+Skin = geompy.MakePrismVecH(Circle, OZ, length)
+
+geompy.addToStudy( O, 'O' )
+geompy.addToStudy( OX, 'OX' )
+geompy.addToStudy( OY, 'OY' )
+geompy.addToStudy( OZ, 'OZ' )
+geompy.addToStudy( Circle, 'Circle' )
+geompy.addToStudy( Skin, 'Skin' )
+
+geompy.ExportSTEP(Skin, ExportPATH+'Skin.step', GEOM.LU_METER )
